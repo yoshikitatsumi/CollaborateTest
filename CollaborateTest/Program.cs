1d6e4d2cb2762c66733c63ad1cf0c919aee456dc
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-11-12 10:35:42 +1300

    Changed text.

--- CollaborateTest/Program.cs
+++ CollaborateTest/Program.cs
@@ -6,7 +6,7 @@ namespace CollaborateTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("This is Yoshi.");
         }
     }
 }
